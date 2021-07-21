8c69fdc4255e552c942cd66e06558fe1979cdadd
kubokohappy <kuboko1201@gamil.com>
2021-07-21 07:02:25 +0000

    roma

--- jukebox/data/sampling_lyrics.py
+++ jukebox/data/sampling_lyrics.py
@@ -6,15 +6,15 @@ import json
 def input_meta(offset:int, total_length:int) -> list:
 
     path_wav = "wav_dataset_005"
-    sampling_no = 0
-    lyric_file_name = "backnumber_クリスマスソング.json"
-    df_sampling_list = pd.read_csv(f"/workspace/dataset/{path_wav}/sampling_list/title_list_{str(sampling_no)}.csv", header=0)
+    sampling_file_name = "1b_lyrics_v3_artist.csv"
+    lyric_file_name = "free_lyric_01.json"
+    df_sampling_list = pd.read_csv(f"/workspace/dataset/{path_wav}/sampling_list/{sampling_file_name}", header=0)
     lyric_path = os.path.join('/', 'workspace', 'dataset', path_wav, 'input_jukebox', lyric_file_name)
 
     # 歌詞を読み込み
     with open(lyric_path, mode='r', encoding='utf-8') as f:
         lyric = json.load(f)
-    lyrics_use = lyric['lyric_hira']
+    lyrics_use = lyric['lyric_roma']
 
     metas = []
     for idx in df_sampling_list.index:
