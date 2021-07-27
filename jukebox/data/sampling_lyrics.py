2cbf99af27ff1c78ffda165290607a36b76ba353
Kuboko <kuboko1201@gmail.com>
2021-07-27 03:17:51 +0000

    mv samples

--- jukebox/data/sampling_lyrics.py
+++ jukebox/data/sampling_lyrics.py
@@ -6,7 +6,7 @@ import json
 def input_meta(offset:int, total_length:int) -> list:
 
     path_wav = "wav_dataset_005"
-    sampling_file_name = "1b_lyrics_v3_artist.csv"
+    sampling_file_name = "title_list_0.csv"
     lyric_file_name = "free_lyric_01.json"
     df_sampling_list = pd.read_csv(f"/workspace/dataset/{path_wav}/sampling_list/{sampling_file_name}", header=0)
     lyric_path = os.path.join('/', 'workspace', 'dataset', path_wav, 'input_jukebox', lyric_file_name)
